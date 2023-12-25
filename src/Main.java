43b827baa60b0b05ab3328f22a90c09964fb7735
ManuOmulo <omuloemmanuel@gmail.com>
2023-12-25 19:13:24 +0300

    Refactored code

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,23 @@
 public class Main {
   public static void main(String[] args) {
     Car ford = new Car("1991 Ford fusion");
-    ford.startEngine();
-    ford.drive();
-    ford.getObjectInstance();
+    runRace(ford);
 
-    System.out.println("------------------------");
-
-    GasPoweredCar chevy = new GasPoweredCar("2021 Chevy Camaro", 12, 8);
-    chevy.startEngine();
-    chevy.drive();
-    chevy.getObjectInstance();
-
-    System.out.println("------------------------");
+    Car chevy = new GasPoweredCar("2021 Chevy Camaro", 12, 8);
+    runRace(chevy);
 
     var tesla = new ElectricCar(550, 2000);
-    tesla.startEngine();
-    tesla.drive();
-    tesla.getObjectInstance();
-
-    System.out.println("------------------------");
+    runRace(tesla);
 
     var hyundai = new HybridCar(18, 1000, 6);
-    hyundai.startEngine();
-    hyundai.drive();
-    hyundai.getObjectInstance();
+    runRace(hyundai);
+  }
+
+  public static void runRace(Car car) {
+    car.startEngine();
+    car.drive();
+    car.getObjectInstance();
+
+    System.out.println("------------------------");
   }
 }
